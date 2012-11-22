8a2d2e67332ca40b6a9887934902b012a7ac5976
f varas <fvaras@smartjog.com>
2012-11-22 15:51:39 +0100

    fixed last stupid error about the logfile name

--- logpyle.py
+++ logpyle.py
@@ -7,18 +7,18 @@
 import time, os, sys
 
 # default values for logger
-#debug=False
 defaultuser=os.getlogin()
 defaultprogram=sys.argv[0]
 defaultlogfile="simple.log"
 
 def logger(debug = False, user = defaultuser, program = defaultprogram, action = "log line"):
+  #debug = True
   NOW=time.strftime('%Y/%m/%d %H:%M:%S ')
   if debug == True:
     print("debug mode on")
-  print("timestap: %s, user: %s, program: %s") %(NOW,user,program)
-  logging = open(logfile,'a')
-  data=("%s %s %s \n") %(NOW,user,program)
+    print("timestap: %s user: %s program: %s action: %s") %(NOW,user,program,action)
+  logging = open(defaultlogfile,'a')
+  data=("%s %s %s %s\n") %(NOW,user,program,action)
   logging.write(data)
   logging.close()
 
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import logpyle, time
 
 # stupid test script
