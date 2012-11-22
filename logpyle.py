f8d7831c672dc7d00d471d8219b3ce96b9576080
f varas <fvaras@smartjog.com>
2012-11-22 14:34:26 +0100

    first working prototype

--- logpyle.py
+++ logpyle.py
@@ -3,8 +3,22 @@
 
 # simple python logging facility
 
-def logger(debug,user,program):
-  NOW="date"
+# python imports
+import time, os, sys
+
+# default values for logger
+#debug=False
+defaultuser=os.getlogin()
+defaultprogram=sys.argv[0]
+defaultlogfile="simple.log"
+
+def logger(debug = False, user = defaultuser, program = defaultprogram, logfile = defaultlogfile):
+  NOW=time.strftime('%Y/%m/%d %H:%M:%S ')
   if debug == True:
     print("debug mode on")
-  
+  print("timestap: %s, user: %s, program: %s") %(NOW,user,program)
+  logging = open(logfile,'a')
+  data=("%s %s %s \n") %(NOW,user,program)
+  logging.write(data)
+  logging.close()
+
